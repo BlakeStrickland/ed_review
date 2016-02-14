19a31baa9eeca2ab368fb283c3826a1e61920b0b
Blake Strickland <Stricklandb02@gmail.com>
2016-02-14 13:02:11 -0500

    Initial commit

---
+++ ed_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'byebug'
+#Note: This line is going to fail first.
+require './battleship.rb'
+require './player.rb'
+
+
+
+class BattleshipTest < Minitest::Test
+
+end
